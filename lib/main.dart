8c9a2195b37c67159b1954befacd0a7efad6f286
Ishan Sharma <ishandeveloper@outlook.com>
2020-07-26 20:03:05 +0530

    app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:hackathon_media_player/pages/videoFeed.dart';
+
+void main() {
+  runApp(MediaPlayerApp());
+}
+
+class MediaPlayerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Media Player',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: VideoFeed(),
+    );
+  }
+}
